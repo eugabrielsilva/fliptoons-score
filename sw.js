cf829c35599aadc0ca06eb00e94b333b6e1c1706
Gabriel Oliveira <gabriel.o.silva10@gmail.com>
2026-05-03 14:49:58 -0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "fliptoons-v1";
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./logo.webp",
+                "./icon.png",
+                "./manifest.json"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
